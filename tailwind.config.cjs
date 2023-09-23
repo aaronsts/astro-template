a6d2ef4ce7f9672206924ce6402f1c9bbfc34196
Aaron <aaron.staes@gmail.com>
2023-09-23 11:28:55 -0400

    feat: added theming + hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,69 +1,70 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			sans: ['Barlow', 'sans-serif'],
+			serif: ["Playfair Display", "serif"],
+			sans: ["Work sans", "sans-serif"],
 		},
 		extend: {
 			colors: {
-				black: '#1F1F1F',
-				white: '#F6F6F6',
-				primary: '#FFB452',
-				accent: '#028090',
+				black: "#0F0905",
+				white: "#F7F5F3",
+				primary: "#B0AA48",
+				accent: "#0F5257",
 			},
 			keyframes: {
 				openclose: {
-					'0%': {
-						top: '0',
+					"0%": {
+						top: "0",
 					},
-					'5%': {
-						top: '0',
+					"5%": {
+						top: "0",
 					},
 
-					'30%': {
-						top: '0',
+					"30%": {
+						top: "0",
 					},
-					'33%': {
-						top: '0',
+					"33%": {
+						top: "0",
 					},
-					'35%': {
-						top: '0',
+					"35%": {
+						top: "0",
 					},
-					'38%': {
-						top: '-3rem',
+					"38%": {
+						top: "-3rem",
 					},
-					'48%': {
-						top: '-3rem',
+					"48%": {
+						top: "-3rem",
 					},
-					'62%': {
-						top: '-3rem',
+					"62%": {
+						top: "-3rem",
 					},
-					'66%': {
-						top: '-3rem',
+					"66%": {
+						top: "-3rem",
 					},
-					'71%': {
-						top: '-6rem',
+					"71%": {
+						top: "-6rem",
 					},
-					'86%': {
-						top: '-6rem',
+					"86%": {
+						top: "-6rem",
 					},
-					'95%': {
-						top: '-6rem',
+					"95%": {
+						top: "-6rem",
 					},
-					'98%': {
-						top: '-6rem',
+					"98%": {
+						top: "-6rem",
 					},
-					'100%': {
-						top: '0',
+					"100%": {
+						top: "0",
 					},
 				},
 			},
 			animation: {
-				openclose: 'openclose 5s ease-in-out infinite',
+				openclose: "openclose 5s ease-in-out infinite",
 			},
 		},
 	},
 	plugins: [],
-}
+};
